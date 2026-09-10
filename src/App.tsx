82a3a0784a998f085bfc5021c4bc5c25f23ef425
raihankabir1952 <raihankabir1952@gmail.com>
2026-09-10 20:09:28 +0600

    nav bar done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './Components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav>
+        
+      </Nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,30 @@
+import myLogoImg from '../assets/logo-text.png'
+
+function Nav() {
+  return (
+    <nav className="w-full px-6 py-4 shadow-sm bg-white-200">
+      <div className="flex justify-between items-center"> 
+        {/* LOGO */}
+        <div className="flex-1 flex justify-start font-bold text-xl">
+            <img src={myLogoImg} alt="" width={150} />
+        </div>
+        
+        {/* middle part */}
+        <div className="flex-1 flex justify-center items-center gap-6">
+          <a href="#" className="hover:text-blue-600  text-pink-700">Home</a>
+          <a href="#" className="hover:text-blue-600">technologies</a>
+          <a href="#" className="hover:text-blue-600">Projects</a>
+          <a href="#" className="hover:text-blue-600">Contact</a>
+        </div>
+        
+        {/* button */}
+        <div className="flex-1 flex justify-end items-center gap-4">
+          <button className="hover:text-blue-600">Sign In</button>
+          <button className="bg-fuchsia-500 text-white px-4 py-2 rounded-full">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
